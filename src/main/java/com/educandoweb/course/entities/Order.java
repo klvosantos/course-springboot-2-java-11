d06c58f52a476f7e75cbd39f4ead4d4c171c0495
klvo <klvo.gtfo.gmail.com>
2020-09-09 16:52:25 -0300

    OrderStatus enum

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -11,10 +11,11 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
+import com.educandoweb.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
-@Table(name = "tb_order") // adicionado name para evitar conflitos com palavras reservadas do sql
+@Table(name = "tb_order") 
 public class Order implements Serializable{
 
 	private static final long serialVersionUID = 1L;
@@ -26,18 +27,21 @@ public class Order implements Serializable{
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
-	@ManyToOne    // classe Order tem associação muitos para 1 com a classe User, instrui o JPA a transformar isso em uma chave estrangeira
-	@JoinColumn(name = "client_id") // informa qual o nome da chave estrangeira que tera no banco de dados
+	private Integer orderStatus; // Este tratamento de numero inteiro é só interno da classe, para dizer explicitamente para o banco de dados que esta sendo gravado um numero inteiro. Para o mundo externo continua sendo o tipo orderStatus 
+	
+	@ManyToOne    
+	@JoinColumn(name = "client_id") 
 	private User client;
 	
 	public Order() {
 		
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -55,6 +59,16 @@ public class Order implements Serializable{
 
 	public void setMoment(Instant moment) {
 		this.moment = moment;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if(orderStatus != null) {
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	public User getClient() {
