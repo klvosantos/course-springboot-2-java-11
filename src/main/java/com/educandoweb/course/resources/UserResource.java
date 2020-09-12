05918b17f22f0c75a3a204d852f58ae45299765d
klvo <klvo.gtfo.gmail.com>
2020-09-12 12:09:33 -0300

    User insert

--- src/main/java/com/educandoweb/course/resources/UserResource.java
+++ src/main/java/com/educandoweb/course/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.educandoweb.course.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.educandoweb.course.Services.UserService;
 import com.educandoweb.course.entities.User;
@@ -25,10 +29,24 @@ public class UserResource {
 		return ResponseEntity.ok().body(list);
 	}
 	
-	@GetMapping(value = "/{id}") // informa que a requisição recebera um parametro dentro da url 
+	@GetMapping(value = "/{id}") 
 	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
 	
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
+		
+		// forma padrão de gerar o endereço location
+		// path() recebe um modelo padrão para a criação da Url
+		// buildAndExpand precisa do id inserido
+		// Altera a msg de resposta para 201 e cria um cabeçalho Location, contendo o endereço e demais informações sobre o novo recurso inserido.
+		// created() recebe o objeto uri contruido seguido do obj inserido no banco de dados
+	}
 }
+
+ 
